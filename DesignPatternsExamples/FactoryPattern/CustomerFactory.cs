ddb462f56a94ee4c542aef1f6606d9cf17cee04c
Jacek Kowalski <Jacek.Kowalski@hyland.com>
2025-12-31 10:17:35 +0100

    customer factory

---
+++ DesignPatternsExamples/FactoryPattern/CustomerFactory.cs
@@ -0,0 +1,16 @@
+﻿namespace FactoryPattern
+{
+    public static class CustomerFactory
+    {
+        private static int _idCounter = 1;
+
+        public static Customer CreateCustomer(string name)
+        {
+            return new Customer
+            {
+                Id = _idCounter++,
+                Name = name
+            };
+        }
+    }
+}
